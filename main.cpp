d21f6953b119f2cf14912174cdeb336714372dbd
Rick van Mourik <43366526+RickvMourik@users.noreply.github.com>
2020-01-28 12:33:53 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,23 +1,29 @@
-// main.cpp
-// Daniel van Eijk-Bos Bulkowski, 02-Dec-19, Version 1.1
-
 // Reads drawable object from a file, allows the user to move them inside the window and saves the new objects to the file
 #include "factory.hpp"
 #include "settings.hpp"
-#include "game.hpp"
 #include <iostream>
-
+//#include "screens.hpp"
+#include "main_menu.hpp"
+#include "game.hpp"
 
 int main(int argc, char* argv[]) {
+	//Applications variables
+	std::vector<Screens*> Screens;
+	int screen = 0;
 	sf::RenderWindow window{ sf::VideoMode{WINDOW_SIZE_X, WINDOW_SIZE_Y}, "epic" };
-	Game platformer(window);
+	//Game platformer(window);
 	window.setFramerateLimit(FRAME_RATE);
-	std::cout << "Starting game..." << std::endl;\
-	while (window.isOpen()) {
-		// Draw all objects
-		platformer.game_get_input();
-		platformer.game_update();
-		platformer.game_draw();
+	std::cout << "Starting game..." << std::endl;
+	main_menu s0(window);
+	Screens.push_back(&s0);
+	Game platformer(window);
+	Screens.push_back(&platformer);
+	//s0.init();
+
+	//Main loop
+	while (screen >= 0) {
+		screen = Screens[screen]->Run();
 	}
-	return 0;
+
+	return EXIT_SUCCESS;
 }
